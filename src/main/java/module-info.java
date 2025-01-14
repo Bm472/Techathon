6c2cc902c45ec3c5adcc401a12b91269482f1fd9
Bradley <94051876+Bm472@users.noreply.github.com>
2025-01-14 15:09:02 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module uk.co.robertwalters.techathon {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens uk.co.robertwalters.techathon to javafx.fxml;
+    exports uk.co.robertwalters.techathon;
+}
